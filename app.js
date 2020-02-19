fa95cb31c1004327f4a87773aab7eb8e331481be
andredante <andre.dante@ramper.com.br>
2020-02-19 19:24:26 -0300

    feat(3): Episode 3: Multiple WebSocket Connections. NodeJs Tutorial Guide

--- app.js
+++ app.js
@@ -9,18 +9,41 @@ app.use('/client',express.static(__dirname + '/client'));
  
 serv.listen(2000);
 console.log("Server started.");
- 
+
+var SOCKET_LIST = {};
  
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket){
-    console.log('socket connection');
- 
-    socket.on('happy',function(data){
-        console.log('happy because ' + data.reason);
-    });
-   
-    socket.emit('serverMsg',{
-        msg:'hello',
-    });
-   
+
+	socket.id = Math.random();
+	socket.x = 0;
+	socket.y = 0;
+	socket.number = "" + Math.floor(10 * Math.random());
+	SOCKET_LIST[socket.id] = socket;
+
+	socket.on('disconnect',function(){
+		delete SOCKET_LIST[socket.id];
+	})
+  
 });
+
+setInterval(function(){
+	var pack = [];
+	
+	for(var i in SOCKET_LIST){
+		var socket = SOCKET_LIST[i];
+		socket.x++;
+		socket.y++;	
+		pack.push({
+			x:socket.x,
+			y:socket.y,
+			number:socket.number
+		})
+	}
+
+	for(var i in SOCKET_LIST){
+		var socket = SOCKET_LIST[i];
+		socket.emit('newPositions',pack)
+	}
+	
+},1000/25)
