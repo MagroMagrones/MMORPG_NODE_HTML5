5931c378b95e0fe037ff7466b1fb762c8936aa1f
andredante <andre.dante@ramper.com.br>
2020-02-20 18:28:19 -0300

    feat(6): Episode 6: Chat & Debug Server

--- app.js
+++ app.js
@@ -136,6 +136,8 @@ Bullet.update = function(){
 	}
 	return pack;
 }
+
+var DEBUG = true;
  
 var io = require('socket.io')(serv,{});
 
@@ -150,6 +152,20 @@ io.sockets.on('connection', function(socket){
 		delete SOCKET_LIST[socket.id];
 		Player.onDisconnect(socket);
 	});  
+
+	socket.on('sendMsgToServer',function(data){
+		var playerName = ("" + socket.id).slice(2,7);
+		for(var i in SOCKET_LIST){
+			SOCKET_LIST[i].emit('addToChat', playerName + ': ' + data);
+		}
+	});  
+	socket.on('evalServer',function(data){
+		if(!DEBUG)
+			return;
+			
+		var res = eval(data);
+		socket.emit('evalAnswer',res);
+	}); 
 });
 
 setInterval(function(){
