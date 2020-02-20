c94555df6ab18b68c8527ddce072647634607cc1
AndreMarques89 <526473+gb96@users.noreply.github.com>
2020-02-20 00:03:33 -0300

    feat(4): Episode 4: Keyboard Interactivity

--- app.js
+++ app.js
@@ -11,33 +11,73 @@ serv.listen(2000);
 console.log("Server started.");
 
 var SOCKET_LIST = {};
+var PLAYER_LIST = {};
+
+var Player = function(id){
+	var self = {
+		x:250,
+		y:250,
+		id:id,
+		number:"" + Math.floor(10 * Math.random()),
+		pressingRight: false,
+		pressingLeft: false,
+		pressingUp: false,
+		pressingDown: false,
+		maxSpd:10,
+	}
+	self.updatePosition = function(){
+		if(self.pressingRight)
+			self.x +=self.maxSpd;
+		if(self.pressingLeft)
+			self.x -=self.maxSpd;
+		if(self.pressingUp)
+			self.y -=self.maxSpd;
+		if(self.pressingDown)
+			self.y +=self.maxSpd;
+	}
+	return self;
+}
+
  
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket){
 
 	socket.id = Math.random();
-	socket.x = 0;
-	socket.y = 0;
-	socket.number = "" + Math.floor(10 * Math.random());
 	SOCKET_LIST[socket.id] = socket;
+
+	var player = Player(socket.id);
+	PLAYER_LIST[socket.id] = player;
 
 	socket.on('disconnect',function(){
 		delete SOCKET_LIST[socket.id];
-	})
+		delete PLAYER_LIST[socket.id];
+	});
+
+	socket.on('keyPress',function(data){
+		if(data.inputId === 'left')
+			player.pressingLeft = data.state;
+		if(data.inputId === 'right')
+			player.pressingRight = data.state;
+		if(data.inputId === 'up')
+			player.pressingUp = data.state;
+		if(data.inputId === 'down')
+			player.pressingDown = data.state;
+	});
+
+
   
 });
 
 setInterval(function(){
 	var pack = [];
 	
-	for(var i in SOCKET_LIST){
-		var socket = SOCKET_LIST[i];
-		socket.x++;
-		socket.y++;	
+	for(var i in PLAYER_LIST){
+		var player = PLAYER_LIST[i];
+		player.updatePosition();
 		pack.push({
-			x:socket.x,
-			y:socket.y,
-			number:socket.number
+			x:player.x,
+			y:player.y,
+			number:player.number
 		})
 	}
 
